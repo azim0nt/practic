3758f37bc9c02ba2e24918184d68fb4ca2a181e3
azim0nt <kktotov457@gmail.com>
2025-01-27 22:12:19 +0500

    working on vue js

---
+++ vue/frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<template>
+<div class="app-wrapper">
+  <Header/>
+</div>
+</template>
+
+<style lang="scss">
+@use './assets/styles/settings.scss' as *;
+
+.app-wrapper{
+    background-color: $bg-color;
+
+}   
+</style>
---
+++ vue/frontend/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="header-wrapper">
+        <nav>
+            <div class="left">
+                <a href="#">GASLUR</a>
+            </div>
+            <div class="middle">
+                <a href="#">HOME</a>
+                <a href="#">MY PROFILE</a>
+                <a href="#">ACTIVITy</a>
+                <a href="#"></a>
+            </div>
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+<style lang="scss" scoped>
+@use '../assets/styles/settings.scss' as *;
+
+.header-wrapper {
+ 
+}
+</style>
